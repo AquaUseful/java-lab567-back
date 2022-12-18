1a79715f7909f32a1122f76dc13088be6cc13ecf
AquaUseful <l.vladik.m@gmail.com>
2022-12-18 21:35:19 +0300

    add: Some service methods for patching users

--- src/main/java/org/psu/lab5/service/UserService.java
+++ src/main/java/org/psu/lab5/service/UserService.java
@@ -1,13 +1,14 @@
 package org.psu.lab5.service;
 
-import java.io.Console;
-
 import org.psu.lab5.authentication.JwtAuthentication;
-import org.psu.lab5.model.BinFile;
 import org.psu.lab5.model.User;
+import org.psu.lab5.pojo.RegisterRequest;
+import org.psu.lab5.pojo.UserPatch;
 import org.psu.lab5.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Collection;
 
 @Service
 public class UserService {
@@ -21,6 +22,10 @@ public class UserService {
     public User getByUsername(String username) {
         return userRepository.getByUsername(username)
                 .orElseThrow(() -> new RuntimeException("User not found"));
+    }
+
+    public Collection<User> getAll() {
+        return userRepository.findAll();
     }
 
     public Boolean existsByUsername(String username) {
@@ -37,6 +42,23 @@ public class UserService {
 
     public void save(User user) {
         userRepository.save(user);
+    }
+
+    public void applyPatchByUsername(String username, UserPatch patch) {
+        User user = this.getByUsername(username);
+        if (patch.getUsername() != null) {
+            user.setUsername(patch.getUsername());
+        }
+        if (patch.getEmail() != null) {
+            user.setEmail(patch.getEmail());
+        }
+        if (patch.getPassword() != null) {
+            user.setPassword(patch.getPassword());
+        }
+        if (patch.getRoles() != null) {
+            user.setRoles(patch.getRoles());
+        }
+        this.save(user);
     }
 
 }
