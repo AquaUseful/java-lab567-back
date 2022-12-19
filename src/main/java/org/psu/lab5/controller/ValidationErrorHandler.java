9a6c852201c638573970395f5e75381ee39aeed9
AquaUseful <l.vladik.m@gmail.com>
2022-12-19 03:31:52 +0300

    add: Exception handler for multipart forms

--- src/main/java/org/psu/lab5/controller/ValidationErrorHandler.java
+++ src/main/java/org/psu/lab5/controller/ValidationErrorHandler.java
@@ -9,6 +9,7 @@ import org.psu.lab5.pojo.ValidationErrorResponse;
 import org.psu.lab5.pojo.ValidationViolation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -19,6 +20,19 @@ public class ValidationErrorHandler {
         @ExceptionHandler(MethodArgumentNotValidException.class)
         public ResponseEntity<ValidationErrorResponse> methodArgumentNotValidExcetionHandler(
                         MethodArgumentNotValidException exception) {
+                List<ValidationViolation> violations = exception.getBindingResult().getFieldErrors().stream()
+                                .map(
+                                                error -> new ValidationViolation(
+                                                                error.getField(),
+                                                                error.getDefaultMessage()))
+                                .collect(Collectors.toList());
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                                .body(new ValidationErrorResponse(violations));
+        }
+
+        @ExceptionHandler(BindException.class)
+        public ResponseEntity<ValidationErrorResponse> methodArgumentNotValidExcetionHandler(
+                        BindException exception) {
                 List<ValidationViolation> violations = exception.getBindingResult().getFieldErrors().stream()
                                 .map(
                                                 error -> new ValidationViolation(
