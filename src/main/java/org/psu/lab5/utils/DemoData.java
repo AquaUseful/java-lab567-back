650e6141704718f94bc86934b283981b6a24190d
AquaUseful <l.vladik.m@gmail.com>
2022-12-19 03:36:43 +0300

    add: More test data

--- src/main/java/org/psu/lab5/utils/DemoData.java
+++ src/main/java/org/psu/lab5/utils/DemoData.java
@@ -5,8 +5,12 @@ import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import org.psu.lab5.model.News;
 import org.psu.lab5.model.Role;
 import org.psu.lab5.model.User;
+import org.psu.lab5.model.Comment;
+import org.psu.lab5.repository.CommentRepository;
+import org.psu.lab5.repository.NewsRepository;
 import org.psu.lab5.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
@@ -20,26 +24,43 @@ public class DemoData implements ApplicationRunner {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    NewsRepository newsRepository;
+
+    @Autowired
+    CommentRepository commentRepository;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
         try {
 
-            userRepository.save(
-                    new User(null,
-                            "admin",
-                            "admin",
-                            "admin@email.com",
-                            Collections.singleton(Role.ADMIN),
-                            null,
-                            0, null, null));
-            userRepository.save(
-                    new User(null,
-                            "user",
-                            "user",
-                            "user@email.com",
-                            Collections.singleton(Role.USER),
-                            null,
-                            0, null, null));
+            User testUser = new User(null,
+                    "user",
+                    "user",
+                    "user@email.com",
+                    Collections.singleton(Role.USER),
+                    null,
+                    0, null);
+            userRepository.save(testUser);
+
+            userRepository.save(new User(null,
+                    "admin",
+                    "admin",
+                    "admin@email.com",
+                    Collections.singleton(Role.ADMIN),
+                    null,
+                    0, null));
+
+            News testNews = new News(
+                    null,
+                    "Тестовое название",
+                    "Тестовое содержание",
+                    0L,
+                    null, null);
+            Comment testComment = new Comment(null, "Тестовый комментарий", testUser, testNews);
+
+            newsRepository.save(testNews);
+            commentRepository.save(testComment);
 
         } catch (DataIntegrityViolationException e) {
 
