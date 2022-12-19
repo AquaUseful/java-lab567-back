3cf7d64d237175ac1c206d825d9993ddffa2f752
AquaUseful <l.vladik.m@gmail.com>
2022-12-19 03:37:21 +0300

    add: Method for adding users witout auth service

--- src/main/java/org/psu/lab5/service/UserService.java
+++ src/main/java/org/psu/lab5/service/UserService.java
@@ -1,14 +1,20 @@
 package org.psu.lab5.service;
 
+import org.apache.catalina.connector.Request;
 import org.psu.lab5.authentication.JwtAuthentication;
+import org.psu.lab5.exception.UserExistsException;
 import org.psu.lab5.model.User;
 import org.psu.lab5.pojo.RegisterRequest;
-import org.psu.lab5.pojo.UserPatch;
+import org.psu.lab5.pojo.NewUserRequest;
 import org.psu.lab5.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
+import java.util.Collections;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
 @Service
 public class UserService {
@@ -44,7 +50,7 @@ public class UserService {
         userRepository.save(user);
     }
 
-    public void applyPatchByUsername(String username, UserPatch patch) {
+    public void applyPatchByUsername(String username, NewUserRequest patch) {
         User user = this.getByUsername(username);
         if (patch.getUsername() != null) {
             user.setUsername(patch.getUsername());
@@ -55,10 +61,26 @@ public class UserService {
         if (patch.getPassword() != null) {
             user.setPassword(patch.getPassword());
         }
-        if (patch.getRoles() != null) {
-            user.setRoles(patch.getRoles());
+        if (patch.getRole() != null) {
+            user.setRoles(Collections.singleton(patch.getRole()));
         }
         this.save(user);
+    }
+
+    public void addUser(@NotNull @Valid NewUserRequest request) throws UserExistsException {
+        if (userRepository.existsByUsername(request.getUsername())) {
+            throw new UserExistsException();
+        }
+        final User newUser = new User(
+                null,
+                request.getUsername(),
+                request.getEmail(),
+                request.getPassword(),
+                Collections.singleton(request.getRole()),
+                null,
+                0,
+                null);
+        this.save(newUser);
     }
 
 }
