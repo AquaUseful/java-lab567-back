ce4f5c0ed65aed861b2f2ec0f11bc297b3cbca1c
AquaUseful <l.vladik.m@gmail.com>
2022-12-21 22:52:16 +0300

    fix: User deletion method in repository

--- src/main/java/org/psu/lab5/repository/UserRepository.java
+++ src/main/java/org/psu/lab5/repository/UserRepository.java
@@ -9,4 +9,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> getByUsername(String username);
 
     Boolean existsByUsername(String username);
+
+    void deleteByUsername(String username);
 }
