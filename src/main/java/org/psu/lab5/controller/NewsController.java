fe9bf4f745a22c6300d57d28c287f6d1d5985095
AquaUseful <l.vladik.m@gmail.com>
2022-12-23 03:17:19 +0300

    add: News attachment path

--- src/main/java/org/psu/lab5/controller/NewsController.java
+++ src/main/java/org/psu/lab5/controller/NewsController.java
@@ -6,6 +6,8 @@ import java.util.Collection;
 import javax.validation.Valid;
 import javax.validation.constraints.Null;
 
+import org.psu.lab5.exception.ResourceNotFoundException;
+import org.psu.lab5.model.BinFile;
 import org.psu.lab5.model.News;
 import org.psu.lab5.pojo.NewNewsRequest;
 import org.psu.lab5.service.NewsService;
@@ -42,6 +44,15 @@ public class NewsController {
     public ResponseEntity<News> getNewsById(@PathVariable("id") Long id) {
         final News news = newsService.getById(id);
         return ResponseEntity.ok().body(news);
+    }
+
+    @GetMapping(path = "/{id}/attachment")
+    public ResponseEntity<byte[]> getNewsAttachmentById(@PathVariable("id") Long id) {
+        final BinFile attachment = newsService.getById(id).getPicture();
+        if (attachment == null) {
+            throw new ResourceNotFoundException("Изображение новости не найдено");
+        }
+        return ResponseEntity.ok().body(attachment.getData());
     }
 
 }
